f63592e96229c77047b67adb436acf9482ad85d7
nestor-remo <nestiray12345@gmail.com>
2024-09-24 21:50:01 -0700

    progress

---
+++ ASS3/grammar.py
@@ -0,0 +1,8 @@
+string = "abbbcaaa$"
+
+col:int = 0
+i:int = 0
+state:int = 0
+
+while (i < len(string)):
+    
--- ASS3/main.py
+++ ASS3/main.py
@@ -9,12 +9,15 @@ for line in file:
 
 file.close()
 
+print(res)
+
 for token in res:
     if token.lower() in reserved:
         print(f"{token} is a reserved word")
-    elif token[0] == "_" or token[0].isalpha():
-        print(f"{token} is an identifier")
     elif token.isdigit():
         print(f"{token} is a number")
+    elif token[0] == "_" or token[0].isalpha() and token[1:].isidentifier():
+        print(f"{token} is an identifier")    
     else:
         print(f"{token} is nothing")
+    
